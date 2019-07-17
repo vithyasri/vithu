afecce7c4b284199a27c9078054ddc652ab16b3d
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-07-17 11:05:52 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,16 @@
+def countDivisors(n) : 
+    
+    count = 0
+  
+    for i in range(1, (int)(math.sqrt(n)) + 2) : 
+        if (n % i == 0) : 
+          
+            if( n // i == i) : 
+                count = count + 1
+            else : 
+                count = count + 2
+  
+    if (count % 2 == 0) : 
+        print("Even") 
+    else : 
+        print("Odd") 
