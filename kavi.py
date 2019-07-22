9882bd6942f117400c6d4fe7e5a69d5d4ac43f99
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-07-22 17:48:58 +0530

    Create kavi.py

---
+++ kavi.py
@@ -0,0 +1,7 @@
+vithya=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+kavi=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+nal=str(input())
+for p in range(20):
+  if(nal==vithya[p]):
+    print(kavi[p])
+    break
