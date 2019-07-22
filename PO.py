12dfa84e65eee897798d18073989511a011e30cb
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-07-22 17:45:44 +0530

    Create PO.py

---
+++ PO.py
@@ -0,0 +1,7 @@
+D=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+H=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+K=str(input())
+for i in range(20):
+  if(K==D[i]):
+    print(H[i])
+    break
